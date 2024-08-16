ebf3ee8704a102e5b66560f2922dcd14b7fab506
Yunus Emre <yunussemreth@gmail.com>
2024-08-16 18:54:41 +0300

    fix

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ class Config:
     SECRET_KEY = 'abea3d1266569db8744a79ecdc36bee43d6a14d1'
     ODOO_URL = 'http://217.160.138.215:8447'
     ODOO_DB = 'odoo_db'
-    ODOO_USERNAME = 'odoo'
-    ODOO_PASSWORD = 'sehlen123'
+    ODOO_USERNAME = 'admin'
+    ODOO_PASSWORD = 'admin'
