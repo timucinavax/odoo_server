1e841abb48b8975460fa13c88028c6556c8ef3db
Yunus Emre <yunussemreth@gmail.com>
2024-08-17 19:01:20 +0300

    fix

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, current_app, flash
 import xmlrpc.client
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import app
 
 @app.route('/')
@@ -10,44 +11,43 @@ def dashboard():
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form.get('email')
-    role = request.form.get('role')
     username = request.form.get('username')
     password = request.form.get('password')
+    role = request.form.get('role')
 
     url = current_app.config['ODOO_URL']
     db = current_app.config['ODOO_DB']
-    admin_username = current_app.config['ODOO_USERNAME']
-    admin_password = current_app.config['ODOO_PASSWORD']
 
     common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
-    uid = common.authenticate(db, admin_username, admin_password, {})
+    uid = common.authenticate(db, current_app.config['ODOO_USERNAME'], current_app.config['ODOO_PASSWORD'], {})
 
     models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
 
-    user_ids = models.execute_kw(db, uid, admin_password, 'custom.user', 'search', [[
-        ('email', '=', email), 
-        ('username', '=', username), 
-        ('password', '=', password),
-        ('role', '=', role)
-    ]])
-    
-    if user_ids:
-        if role == 'admin':
-            return redirect(url_for('admin_panel'))
-        elif role == 'user':
-            return redirect(url_for('user_panel'))
-        elif role == 'agency':
-            return redirect(url_for('agency_panel'))
+    user = models.execute_kw(db, uid, current_app.config['ODOO_PASSWORD'], 'custom.user', 'search_read', [[('email', '=', email), ('username', '=', username), ('role', '=', role)]], {'limit': 1})
+
+    if user:
+        stored_password_hash = user[0]['password']
+        if check_password_hash(stored_password_hash, password):
+            if role == 'admin':
+                return redirect(url_for('admin_panel'))
+            elif role == 'user':
+                return redirect(url_for('user_panel'))
+            elif role == 'agency':
+                return redirect(url_for('agency_panel'))
+        else:
+            flash("Hatalı şifre, lütfen tekrar deneyin.")
     else:
-        flash("Giriş bilgileri hatalı, lütfen tekrar deneyin.")
-        return redirect(url_for('dashboard'))
+        flash("Kullanıcı bulunamadı.")
+    return redirect(url_for('dashboard'))
 
 @app.route('/register', methods=['POST'])
 def register():
-    role = request.form.get('role')
     email = request.form.get('email')
     username = request.form.get('username')
     password = request.form.get('password')
+    role = request.form.get('role')
+
+    hashed_password = generate_password_hash(password)
 
     url = current_app.config['ODOO_URL']
     db = current_app.config['ODOO_DB']
@@ -62,7 +62,7 @@ def register():
     user_id = models.execute_kw(db, uid, admin_password, 'custom.user', 'create', [{
         'email': email,
         'username': username,
-        'password': password,
+        'password': hashed_password,
         'role': role,
     }])
 
@@ -75,18 +75,7 @@ def register():
 
 @app.route('/admin')
 def admin_panel():
-    url = current_app.config['ODOO_URL']
-    db = current_app.config['ODOO_DB']
-    username = current_app.config['ODOO_USERNAME']
-    password = current_app.config['ODOO_PASSWORD']
-
-    common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
-    uid = common.authenticate(db, username, password, {})
-
-    models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
-
-    tasks = models.execute_kw(db, uid, password, 'project.task', 'search_read', [[]], {})
-    return render_template('admin_panel.html', tasks=tasks)
+    return render_template('admin_panel.html')
 
 @app.route('/user')
 def user_panel():
