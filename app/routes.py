925adb2edff6b1efec3babb5e24d2cab12bd9665
Yunus Emre <yunussemreth@gmail.com>
2024-08-19 18:01:08 +0300

    fix

--- app/routes.py
+++ app/routes.py
@@ -142,7 +142,7 @@ def admin_panel():
 @role_required(['admin'])
 def add_flight():
     flight_code = request.form.get('flight_code')
-    passenger_count = request.form.get('passenger_count')
+    passenger_count = int(request.form.get('passenger_count'))
     departure = request.form.get('departure')
     arrival = request.form.get('arrival')
     departure_time = request.form.get('departure_time')
@@ -176,11 +176,11 @@ def add_flight():
 
     if flight_id:
         flash('Flight added successfully.')
-        return redirect(url_for('plane_layout', flight_id=flight_id))
     else:
         flash('An error occurred while adding the flight.')
 
     return redirect(url_for('admin_panel'))
+
 
 @app.route('/user')
 @role_required(['user'])
@@ -195,34 +195,6 @@ def agency_panel():
 @app.route('/sign')
 def sign():
     return render_template('sign.html')
-
-@app.route('/plane_layout/<int:flight_id>', methods=['GET', 'POST'])
-@role_required(['admin'])
-def plane_layout(flight_id):
-    url = current_app.config['ODOO_URL']
-    db = current_app.config['ODOO_DB']
-    admin_username = current_app.config['ODOO_USERNAME']
-    admin_password = current_app.config['ODOO_PASSWORD']
-
-    common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common', allow_none=True)
-    uid = common.authenticate(db, admin_username, admin_password, {})
-
-    models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object', allow_none=True)
-
-    flight = models.execute_kw(db, uid, admin_password, 'flight.management', 'read', [[flight_id]], {'fields': ['flight_number', 'airplane_type_id', 'seat_ids']})
-    airplane_type_id = flight[1]
-
-    seats = models.execute_kw(db, uid, admin_password, 'flight.seat', 'search_read', [[('flight_id', '=', flight_id)]], {'fields': ['name', 'user_id']})
-
-    if request.method == 'POST':
-        selected_seats = request.form.getlist('selected_seats')
-        for seat_id in selected_seats:
-            models.execute_kw(db, uid, admin_password, 'flight.seat', 'write', [[int(seat_id)], {'user_id': False}]) 
-        flash('Seats assigned successfully.')
-        return redirect(url_for('admin_panel'))
-
-    return render_template('plane_rev.html', airplane_type_id=airplane_type_id, seats=seats)
-
 
 @app.route('/autocomplete_airport')
 def autocomplete_airport():
