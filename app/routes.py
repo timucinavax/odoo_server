12abb270b7ba8f4d0a0d1e77f24698d8253a06cd
Yunus Emre <yunussemreth@gmail.com>
2024-08-19 17:43:44 +0300

    fix

--- app/routes.py
+++ app/routes.py
@@ -209,9 +209,13 @@ def plane_layout(flight_id):
 
     models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object', allow_none=True)
 
+    # Uçuş bilgilerini alın
     flight = models.execute_kw(db, uid, admin_password, 'flight.management', 'read', [[flight_id]], {'fields': ['flight_number', 'airplane_type_id', 'seat_ids']})
     
-    airplane_type = flight[0]['airplane_type_id'][0]  # Uçak tipi ID'sini al
+    # Uçak tipi ID yerine string olarak alınır
+    airplane_type_string = flight[0]['airplane_type_id'][1]  # İkinci eleman string değerini içerir
+
+    # Koltuk bilgilerini alın
     seats = models.execute_kw(db, uid, admin_password, 'flight.seat', 'search_read', [[('flight_id', '=', flight_id)]], {'fields': ['name', 'user_id']})
 
     if request.method == 'POST':
@@ -222,7 +226,8 @@ def plane_layout(flight_id):
         flash('Seats assigned successfully.')
         return redirect(url_for('admin_panel'))
 
-    return render_template('plane_rev.html', airplane_type=airplane_type, seats=seats)
+    return render_template('plane_rev.html', airplane_type=airplane_type_string, seats=seats)
+
 
 @app.route('/autocomplete_airport')
 def autocomplete_airport():
