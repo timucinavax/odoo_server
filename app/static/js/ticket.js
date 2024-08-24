b4158dfacd31533fe6646106c0b999544ce0a913
Yunus Emre <yunussemreth@gmail.com>
2024-08-24 20:35:23 +0300

    s

--- app/static/js/ticket.js
+++ app/static/js/ticket.js
@@ -1,3 +1,5 @@
+let selectedFlight = null;
+
 function showFlights(date) {
     document.querySelectorAll('.date-box').forEach(box => box.classList.remove('active'));
     document.querySelector(`.date-box[onclick="showFlights('${date}')"]`).classList.add('active');
@@ -14,38 +16,25 @@ function displayFlights(flights) {
         const flightDuration = calculateFlightDuration(flight.departure_time, flight.arrival_time);
         const flightCard = `
             <div class="card">
-                <div class="card-left">
-                    <div class="time-info">
-                        <p><span class="heading">Uçuş Numarası:</span> ${flight.flight_number}</p>
-                    </div>
-                    <div class="time-info">
-                        <p>${flight.departure_time.split(" ")[1]}</p>
-                        <p>${flight.departure_airport}</p>
-                    </div>
-                    <div class="route">
-                        <div class="route-line"></div>
-                        <div class="route-logo"></div>
-                        <div class="route-line"></div>
-                    </div>
-                    <div class="time-info">
-                        <p>${flight.arrival_time.split(" ")[1]}</p>
-                        <p>${flight.arrival_airport}</p>
-                    </div>
-                    <div class="time-info">
-                        <p><span class="heading">Fiyat:</span> ${flight.price} TL</p>
-                        <p><span class="heading">Süre:</span> ${flightDuration}</p>
-                    </div>
+                <div class="time-info">
+                    <p><span class="heading">Uçuş Numarası:</span> ${flight.flight_number}</p>
                 </div>
-                <div class="card-right">
-                    <button class="details-button" onclick="toggleDetails(this)">Seyahat Detayları</button>
-                    <div class="details-content">
-                        <p><span class="heading">Uçuş Numarası:</span> ${flight.flight_number}</p>
-                        <p><span class="heading">Kalkış Zamanı:</span> ${flight.departure_time}</p>
-                        <p><span class="heading">Varış Zamanı:</span> ${flight.arrival_time}</p>
-                        <p><span class="heading">Fiyat:</span> ${flight.price} TL</p>
-                        <p><span class="heading">Uçuş Yönü:</span> ${flight.flight_direction}</p>
-                        <p><span class="heading">Koltuk Sayısı:</span> ${flight.available_seats}</p>
-                    </div>
+                <div class="time-info">
+                    <p>${flight.departure_time.split(" ")[1]}</p>
+                    <p>${flight.departure_airport}</p>
+                </div>
+                <div class="route">
+                    <div class="route-line"></div>
+                    <div class="route-logo"></div>
+                    <div class="route-line"></div>
+                </div>
+                <div class="time-info">
+                    <p>${flight.arrival_time.split(" ")[1]}</p>
+                    <p>${flight.arrival_airport}</p>
+                </div>
+                <div class="time-info">
+                    <p><span class="heading">Fiyat:</span> ${flight.price} TL</p>
+                    <p><span class="heading">Süre:</span> ${flightDuration}</p>
                 </div>
                 <div class="time-info">
                     <button class="buy-ticket-button" onclick="buyTicket('${flight.flight_number}')">Bileti Al</button>
@@ -74,28 +63,29 @@ function calculateFlightDuration(departureTime, arrivalTime) {
 }
 
 function buyTicket(flightNumber) {
-    const selectedFlight = flightsData.find(flight => flight.flight_number === flightNumber);
+    selectedFlight = flightsData.find(flight => flight.flight_number === flightNumber);
 
     if (selectedFlight) {
         document.getElementById('flight-number-summary').textContent = selectedFlight.flight_number;
         document.getElementById('departure-summary').textContent = `${selectedFlight.departure_airport} - ${selectedFlight.departure_time}`;
         document.getElementById('arrival-summary').textContent = `${selectedFlight.arrival_airport} - ${selectedFlight.arrival_time}`;
         document.getElementById('price-summary').textContent = selectedFlight.price;
-    }
 
-    document.getElementById('confirmFlightNumber').textContent = flightNumber;
-    document.getElementById('confirmationModal').style.display = 'block';
+        proceedToPassengerInfo();
+    }
 }
 
 function proceedToPassengerInfo() {
-    document.querySelectorAll('.breadcrumb-item').forEach(item => item.classList.remove('active'));
-    const passengerInfoStep = document.querySelector('.breadcrumb-item[data-number="1"]');
-    passengerInfoStep.classList.add('active');
-    
-    document.querySelector('.date-selector').style.display = 'none';
-    document.querySelector('.flights-container').style.display = 'none';
-    document.querySelector('#passenger-info-form').style.display = 'block';
-    document.getElementById('confirmationModal').style.display = 'none';
+    if (selectedFlight) {
+        document.querySelectorAll('.breadcrumb-item').forEach(item => item.classList.remove('active'));
+        document.querySelector('.breadcrumb-item[data-number="1"]').classList.add('active');
+
+        document.querySelector('.date-selector').style.display = 'none';
+        document.querySelector('.flights-container').style.display = 'none';
+        document.querySelector('.passenger-info-container').style.display = 'block';
+    } else {
+        alert("Lütfen önce bir uçuş seçin.");
+    }
 }
 
 function closeModal() {
@@ -104,12 +94,11 @@ function closeModal() {
 
 function goBackToFlightSelection() {
     document.querySelectorAll('.breadcrumb-item').forEach(item => item.classList.remove('active'));
-    const flightSelectionStep = document.querySelector('.breadcrumb-item[data-number="0"]');
-    flightSelectionStep.classList.add('active');
+    document.querySelector('.breadcrumb-item[data-number="0"]').classList.add('active');
 
     document.querySelector('.date-selector').style.display = 'block';
     document.querySelector('.flights-container').style.display = 'block';
-    document.querySelector('#passenger-info-form').style.display = 'none';
+    document.querySelector('.passenger-info-container').style.display = 'none';
 }
 
 document.addEventListener("DOMContentLoaded", function() {
