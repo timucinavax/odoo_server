4f58e3cdfd6bdcab2d6e2e920c73db96bd25034b
Yunus Emre <yunussemreth@gmail.com>
2024-08-25 21:49:44 +0300

    fix

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -1,3 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    showSection('all-flights');
+
+    const currentDate = new Date().setHours(0, 0, 0, 0);
+    const activeFlightsBody = document.getElementById('active-flights-body');
+
+    fetch('/search_flights')
+        .then(response => response.json())
+        .then(flights => {
+            flights.forEach(flight => {
+                const flightDate = new Date(flight.date).setHours(0, 0, 0, 0);
+
+                if (flightDate >= currentDate) {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${flight.flight_number}</td>
+                        <td>${flight.svc_type}</td>
+                        <td>${flight.departure_airport}</td>
+                        <td>${flight.arrival_airport}</td>
+                        <td>${flight.date}</td>
+                    `;
+                    activeFlightsBody.appendChild(row);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Uçuş verileri alınırken bir hata oluştu:', error);
+        });
+});
+
 function showSection(sectionId) {
     const sections = document.querySelectorAll('.content-section');
     sections.forEach(section => section.style.display = 'none');
@@ -17,33 +47,3 @@ function showSection(sectionId) {
         behavior: 'smooth'
     });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  showSection('all-flights');
-
-  const currentDate = new Date().setHours(0, 0, 0, 0);
-
-  {% if flights is defined and flights %}
-  const flights = {{ flights | tojson }};
-  {% else %}
-  const flights = [];
-  {% endif %}
-
-  const activeFlightsBody = document.getElementById('active-flights-body');
-
-  flights.forEach(flight => {
-      const flightDate = new Date(flight.date).setHours(0, 0, 0, 0);
-
-      if (flightDate >= currentDate) {
-          const row = document.createElement('tr');
-          row.innerHTML = `
-              <td>${flight.flight_number}</td>
-              <td>${flight.svc_type}</td>
-              <td>${flight.departure_airport}</td>
-              <td>${flight.arrival_airport}</td>
-              <td>${flight.date}</td>
-          `;
-          activeFlightsBody.appendChild(row);
-      }
-  });
-});
