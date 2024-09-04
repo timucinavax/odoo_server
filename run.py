451ea5636ae6b61b894a8f048caff107d9f449f9
Yunus Emre <yunussemreth@gmail.com>
2024-09-04 17:34:00 +0300

    z

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from app import app
 from babel.dates import format_datetime
+import logging
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.jinja_env.filters['format_datetime'] = format_datetime
+
+logging.basicConfig(filename='flask_app.log', level=logging.INFO)
 
 if __name__ == '__main__':
     app.run(debug=True ,host='0.0.0.0', port="8450")
