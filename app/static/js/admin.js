81b5bfe2f6d0178eb078e254acb5ed48ea000907
Yunus Emre <yunussemreth@gmail.com>
2024-09-05 00:57:07 +0300

    s

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -1,12 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     showSection('all-flights');
+    activateFlight();
+});
 
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => section.style.display = 'none');
+    const sectionToShow = document.getElementById(sectionId);
+    sectionToShow.style.display = 'block';
+
+    const navItems = document.querySelectorAll('.navbar-nav .nav-link');
+    navItems.forEach(item => item.classList.remove('active'));
+
+    const activeNavItem = document.querySelector(`[onclick="showSection('${sectionId}')"]`);
+    if (activeNavItem) {
+        activeNavItem.classList.add('active');
+    }
+
+    window.scrollTo({
+        top: sectionToShow.offsetTop - document.querySelector('.navbar').offsetHeight,
+        behavior: 'smooth'
+    });
+}
+
+function activateFlight() {
     const currentDate = new Date();
 
     fetch('/flight_admin')
         .then(response => response.json())
         .then(flights => {
-            console.log(flights); 
+            console.log(flights);
             if (!flights || flights.length === 0) {
                 console.error('Uçuş verisi alınamadı veya boş geldi.');
                 return;
@@ -32,25 +55,4 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => {
             console.error('Uçuş verileri alınırken bir hata oluştu:', error);
         });
-});
-
-
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => section.style.display = 'none');
-    const sectionToShow = document.getElementById(sectionId);
-    sectionToShow.style.display = 'block';
-
-    const navItems = document.querySelectorAll('.navbar-nav .nav-link');
-    navItems.forEach(item => item.classList.remove('active'));
-
-    const activeNavItem = document.querySelector(`[onclick="showSection('${sectionId}')"]`);
-    if (activeNavItem) {
-        activeNavItem.classList.add('active');
-    }
-
-    window.scrollTo({
-        top: sectionToShow.offsetTop - document.querySelector('.navbar').offsetHeight,
-        behavior: 'smooth'
-    });
 }
