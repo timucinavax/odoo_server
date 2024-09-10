e8b5fdd0b05820d86f4e21b90a615b7350b5a6e1
Yunus Emre <yunussemreth@gmail.com>
2024-09-10 23:39:18 +0300

    s

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,7 @@ from babel.dates import format_datetime
 
 app = Flask(__name__)
 
-def safe_format_datetime(value):
-    try:
-        return format_datetime(value, 'dd MMMM yyyy HH:mm', locale='tr_TR')
-    except Exception as e:
-        return value
-
-app.jinja_env.filters['format_datetime'] = safe_format_datetime
+app.jinja_env.filters['format_datetime'] = lambda value: format_datetime(value, 'dd MMMM yyyy HH:mm', locale='tr_TR')
 app.config.from_mapping(
     SECRET_KEY = 'abea3d1266569db8744a79ecdc36bee43d6a14d1',
     ODOO_URL='http://217.160.138.215:8447',
